a3dcb983c6520b1185a03864a24fab1e8a6d689d
Karl TAYOU <nelsontayou1@gmail.com>
2019-10-05 23:59:13 +0100

    update

--- projet laravel/inialise_bd.sh
+++ projet laravel/inialise_bd.sh
@@ -1,17 +1,68 @@
 #!/bin/bash
-db = mali
-sudo -u postgres psql
-CREATE DATABASE $db;
-$db;
-CREATE EXTENSION postgis;
-CREATE EXTENSION postgis_topology;
-\q
-psql -U username -d $db -f ./BD/template_bd.backup --set ON_ERROR_STOP=on &&
-wget http://download.geofabrik.de/africa/mali-latest.osm.pbf &&
-osm2pgsql --slim -G -c -U postgres -d $db -H localhost -W --hstore-all -S ./BD/default.style mali-latest.osm.pbf &&
-ogr2ogr -f "PostgreSQL" PG:"host=myserver user=postgres dbname==$db password=postgres" shapefile.shp -nln temp_table
-sudo -u postgres psql
-$db;
-UPDATE instances_gc SET boundary = geom, true_geom = geom WHERE id = 1 FROM temp_table limit 1; 
-TRUNCATE stage_table; 
-# ogr2ogr -f "PostgreSQL" "PG:host=127.0.0.1 user=postgres dbname=$db password=postgres" "roi.shp" -lco GEOMETRY_NAME=the_geom -lco FID=gid -lco PRECISION=no -nlt PROMOTE_TO_MULTI -nln new_layername -overwrite
+db="mali"
+roi="../mali.shp"
+path_pbf="http://download.geofabrik.de/africa/mali-latest.osm.pbf"
+list_projet='/var/www/backend_nodejs/projet.json'
+
+
+mkdir -m 777 -p /var/www/geosm/$db/gpkg/
+mkdir -m 777 -p /var/www/geosm/$db/style/
+
+svn checkout  https://github.com/GeoOSM/backend_nodejs/trunk/python_script/style_default
+cp ./style_default/*.qml /var/www/geosm/$db/style/
+rm -r style_default
+
+node > out_${list_projet} <<EOF
+//Read data
+var data = require('./${list_projet}');
+
+//Manipulate data 
+data[$db]={};
+data[$db]['destination_style']='/var/www/geosm/$db/style/';
+data[$db]['destination']='/var/www/geosm/$db/gpkg/';
+data[$db]['database']='$db';
+
+//Output data
+# console.log(JSON.stringify(data));
+EOF
+exit
+
+psql -c "CREATE DATABASE $db"
+echo "db created"
+psql -d  $db -c "CREATE EXTENSION postgis"
+psql -d $db -c "CREATE EXTENSION hstore"
+#psql CREATE EXTENSION postgis_topology
+echo "extention created"
+pg_restore -U postgres -d $db  ./BD/template_bd.backup --verbose
+wget $path_pbf -O osm.pbf
+echo "import termine et telechargement du osm.pbf"
+osm2pgsql --slim -G -c -U postgres -d $db -H localhost -W --hstore-all -S ./BD/default.style osm.pbf
+echo "import du osm.pbf termine"
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=$db password=postgres"  $roi -nln temp_table -lco GEOMETRY_NAME=geom
+psql -d $db -c "UPDATE instances_gc SET geom = st_transform(limite.geom,4326), true_geom = st_transform(limite.geom,4326) FROM (SELECT * from temp_table limit 1) as limite WHERE instances_gc.id = 1;"
+psql -d $db -c "TRUNCATE temp_table;"
+echo "termne !!!!! !!! !"
+exit
+
+
+# sudo apt-get install php7.3-xml
+# sudo apt-get install php-mbstring
+# composer install
+# sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+# sudo apt-get install gdal-bin
+
+#vim /etc/apt/sources.list
+# deb     https://qgis.org/debian buster main
+# deb-src https://qgis.org/debian buster main
+# wget -O - https://qgis.org/downloads/qgis-2019.gpg.key | gpg --import
+# gpg --fingerprint 51F523511C7028C3
+# gpg --export --armor 51F523511C7028C3 | sudo apt-key add -
+# apt install qgis-server
+
+#sudo apt-get install subversion
+# 2a01:e0d:1:c:58bf
+# @2a01:e0d:1:c:58bf:fac1:8000:167
+
+# http://[2a01:e0d:1:c:58bf:fac1:8000:167]
+
+# [2607:f0d0:1002:11::4:80]
--- python+nodejs/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    path_nodejs:'./',
-    path_script_python:'./python_script/',
-    path_projet_qgis:'./',
-    path_style_qml:'/var/www/smartworld/style/',
-    url_qgis_server:'http://tiles.geocameroun.xyz/cgi-bin/qgis_mapserv.fcgi?map=',
-    url_node_js:'http://service.geocameroun.cm/',
-    path_for_download_result:'/home/admin237/analyse/'
-}
